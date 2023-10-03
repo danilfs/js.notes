48476ad3a8148eb141d32ac89275c26efcac1868
dsfrantsev <fds@avito.ru>
2023-10-03 08:35:04 +0300

    second commit - кнопки

--- App.js
+++ App.js
@@ -2,32 +2,41 @@ const inputElement = document.getElementById('title')
 const createBtn = document.getElementById('create')
 const listElement = document.getElementById('list')
 
-const notes = ['deal 1', 'deal 2']
+// const notes = ['deal 1', 'deal 2']
 
-const render = () => {
+const notes = [{
+    title: 'first deal',
+    completed: true,
+},{
+    title: 'second deal',
+    completed: true,
+}
+]
 
-        // for(let i = 0; i < notes.length ; i++) {
-        //     listElement.insertAdjacentHTML('beforeend',getNotesTemplate(notes[i]))
-        // }
+function render () {
 
-        // for (notes of note) {
+        for(let i = 0; i < notes.length ; i++) {
+            listElement.insertAdjacentHTML('beforeend',getNotesTemplate(notes[i], i))
+        }
+
+        // for ( let note of notes) {
         //     listElement.insertAdjacentHTML('beforeend',getNotesTemplate(note))
         // }
 
-    listElement.insertAdjacentHTML('beforeend',
+    // listElement.insertAdjacentHTML('beforeend',
 //         `<ul id="list">
 // <li>
 //     <span>${notes[1]}</span>
 // </li>
 // </ul>`)
-    getNotesTemplate(notes[1]))
-    listElement.insertAdjacentHTML('beforeend',
+    // getNotesTemplate(notes[1]))
+    // listElement.insertAdjacentHTML('beforeend',
 //         `<ul id="list">
 // <li> 
 //     <span>${notes[0]}</span>
 // </li>
 // </ul>`)
-    getNotesTemplate(notes[0]))
+    // getNotesTemplate(notes[0]))
 
 }
 render()
@@ -36,6 +45,11 @@ createBtn.onclick = () => {
     if (inputElement.value.lenght === 0) {
         return
     }
+
+    const newNote = {
+        title : inputElement.value ,
+        completed : false
+    }
     listElement.insertAdjacentHTML('beforeend',
 
 //         `<ul id="list">
@@ -43,7 +57,7 @@ createBtn.onclick = () => {
 //         <span>${inputElement.value}</span>
 //     </li>
 // </ul>`
-        getNotesTemplate(inputElement.value)
+        getNotesTemplate(newNote)
 
     )
 
@@ -52,15 +66,17 @@ createBtn.onclick = () => {
 
 // Не работает почему-то функция getNotesTemplate
 
-function getNotesTemplate(title) {
+function getNotesTemplate(note , index) {
     return `<li
     class="list-group-item d-flex justify-content-between align-items-center"
   >
-    <span>${title}</span>
+    <span class = "${note.completed ? 'text-decoration-line-through' : ''}">${note.title}</span>
     <span>
-      <span class="btn btn-small btn-success">&check;</span>
+      <span class="btn btn-small btn-${note.completed ? 'warning' : 'success'}" data-index="${index}">&check;</span>
       <span class="btn btn-small btn-danger">&times;</span>
     </span>
   </li>`
 }
-const a = 1;
+
+
+
